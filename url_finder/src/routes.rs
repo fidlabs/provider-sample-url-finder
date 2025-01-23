8ae5129b4b1b501d51d7591901da4b963f4b02c4
Michał <5695123+CodeWarriorr@users.noreply.github.com>
2025-01-23 17:22:04 +0100

    rate limiter with two configurations (#6)

---
+++ url_finder/src/routes.rs
@@ -0,0 +1,98 @@
+use std::{sync::Arc, time::Duration};
+
+use axum::{body::Body, http::Response, response::IntoResponse, routing::get, Router};
+use common::api_response::*;
+use tower_governor::{
+    governor::GovernorConfigBuilder, key_extractor::SmartIpKeyExtractor, GovernorError,
+    GovernorLayer,
+};
+use utoipa::OpenApi;
+use utoipa_swagger_ui::SwaggerUi;
+
+use crate::{api::*, AppState};
+
+fn too_many_requests_error_handler(error: GovernorError) -> Response<Body> {
+    tracing::error!("Rate limit error: {:?}", error);
+
+    match error {
+        GovernorError::TooManyRequests { .. } => {
+            too_many_requests("Rate limit exceeded").into_response()
+        }
+        _ => internal_server_error("Rate limit error").into_response(),
+    }
+}
+
+pub fn create_routes() -> Router<Arc<AppState>> {
+    // more strict rate limiting for the sync routes
+    let governor_secure = Arc::new(
+        GovernorConfigBuilder::default()
+            .per_second(4)
+            .burst_size(2)
+            .key_extractor(SmartIpKeyExtractor)
+            .error_handler(too_many_requests_error_handler)
+            .finish()
+            .unwrap(),
+    );
+
+    // less strict rate limiting for the async routes that will have internal queue anyway
+    let governor_async = Arc::new(
+        GovernorConfigBuilder::default()
+            .per_second(1)
+            .burst_size(10)
+            .key_extractor(SmartIpKeyExtractor)
+            .error_handler(too_many_requests_error_handler)
+            .finish()
+            .unwrap(),
+    );
+
+    let governor_secure_limiter = governor_secure.limiter().clone();
+    let governor_async_limiter = governor_async.limiter().clone();
+
+    let interval = Duration::from_secs(60);
+
+    // background task to clean up
+    tokio::spawn(async move {
+        loop {
+            tokio::time::sleep(interval).await;
+
+            tracing::debug!(
+                "rate governor_secure_limiter storage size: {}",
+                governor_secure_limiter.len()
+            );
+            tracing::debug!(
+                "rate governor_async_limiter storage size: {}",
+                governor_async_limiter.len()
+            );
+
+            governor_secure_limiter.retain_recent();
+            governor_async_limiter.retain_recent();
+        }
+    });
+
+    let swagger_routes = SwaggerUi::new("/").url("/api-doc/openapi.json", ApiDoc::openapi());
+
+    let sync_routes = Router::new()
+        .route("/url/find/:provider", get(handle_find_url_sp))
+        .route(
+            "/url/find/:provider/:client",
+            get(handle_find_url_sp_client),
+        )
+        .route(
+            "/url/retrievability/:provider/:client",
+            get(handle_find_retri_by_client_and_sp),
+        )
+        .layer(GovernorLayer {
+            config: governor_secure.clone(),
+        });
+
+    let healthcheck_route = Router::new()
+        .route("/healthcheck", get(handle_healthcheck))
+        .layer(GovernorLayer {
+            config: governor_secure.clone(),
+        });
+
+    Router::new()
+        .merge(swagger_routes)
+        .merge(sync_routes)
+        .merge(healthcheck_route)
+}
